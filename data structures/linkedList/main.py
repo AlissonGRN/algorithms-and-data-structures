2331e983ef5eaf8dac634c0e554d8238466c3527
AlissonGRN <guilhermeribeiro099@gmail.com>
2024-01-03 13:58:51 -0300

    add addToEnd function to the linked list estructure

--- data structures/linkedList/main.py
+++ data structures/linkedList/main.py
@@ -16,4 +16,16 @@ class LinkedList:
     def addToEnd(self, data):
         '''Add a node to the end of the list'''
 
-        pass
+        newNode = Node(data)
+
+        if not self.head: # if the list is empity(or if the head is set to none) the new node becomes the head
+            self.head = newNode
+            return
+        
+        current = self.head
+
+        while current.next: # loop the list until the last node is found
+            current = current.next
+        
+        current.next = newNode
+
